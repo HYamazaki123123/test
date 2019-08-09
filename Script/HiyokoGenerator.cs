aa9eefe0a7e9677e5c58a8ebcec6a8c0f4603d38
HYamazaki123123 <53923173+HYamazaki123123@users.noreply.github.com>
2019-08-09 11:57:34 +0900

    Add files via upload

---
+++ Script/HiyokoGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiyokoGenerator : MonoBehaviour
+{
+    public GameObject Obj;
+    public float interval = 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("SpawnObj", 0.1f, interval);   
+    }
+
+    // Update is called once per frame
+    void SpawnObj ()
+    {
+        Instantiate(Obj, transform.position, transform.rotation);
+    }
+}
